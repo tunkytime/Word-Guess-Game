40854bfbe9de14554bb01979d30c00a8bab1fcd5
Alex <43651369+tunkytime@users.noreply.github.com>
2019-02-24 16:17:39 -0700

    changed game to object and added music

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,7 +6,7 @@ var letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var countGuesses;
 var answer = [];
 var lettersGuessed = [];
-var guess;
+var guess = "";
 
 // ARRAY OF WORDS AND IMAGES
 var words = [{
@@ -51,7 +51,7 @@ var words = [{
 	},
 ]
 
-// BACKGROUNDS TO BE USED
+// BACKGROUNDS
 var backgrounds = [
 	'assets/images/bg2.jpg',
 	'assets/images/bg3.jpg',
@@ -60,7 +60,7 @@ var backgrounds = [
 	'assets/images/bg6.jpg',
 ];
 
-// MESSAGES TO DISPLAY
+// MESSAGES
 var messages = {
 	incorrect: "You're getting closer to the black hole...",
 	correct: "Brilliant!",
@@ -69,7 +69,7 @@ var messages = {
 	notLetter: "Please enter a letter from A-Z",
 };
 
-// SOUNDS TO PLAY
+// SOUNDS
 var playCorrect = document.createElement("audio");
 playCorrect.setAttribute("src", "assets/sounds/deepscan.mp3");
 
@@ -82,159 +82,163 @@ playVoice.setAttribute("src", "assets/sounds/lose-voice.wav");
 var playYell = document.createElement("audio");
 playYell.setAttribute("src", "assets/sounds/yell.wav");
 
-function loseMusic() {
-	var x = document.createElement("audio");
-	x.setAttribute("src", "assets/sounds/lose-music.wav");
-	x.setAttribute("loop", "true");
-	x.play();
-};
+// Gets Link for Theme Song
+var themeSong = document.createElement("audio");
+themeSong.setAttribute("src", "assets/sounds/theme.mp3");
 
-function winMusic() {
-	var x = document.createElement("audio");
-	x.setAttribute("src", "assets/sounds/win-music.mp3");
-	x.setAttribute("loop", "true");
-	x.play();
-}
+$(".begin-button").on("click", function () {
+	themeSong.play();
+});
+
+$(".theme-button").on("click", function () {
+	themeSong.play();
+});
+
+// Theme Button
+$(".pause-button").on("click", function () {
+	themeSong.pause();
+});
 
 $(document).ready(function () {
 
-	// SHOW MODAL WITH GAME INSTRUCTIONS
+	var game = {
+		changeHint: function () {
+			$("#hintImage").attr("src", currentWord.img);
+		},
+		newWord: function () {
+			answer = [];
+			lettersGuessedArr = [];
+			lettersGuessed = lettersMatched = "";
+			countLettersMatched = 0;
+			countGuesses = 25;
+
+			// UPDATE STATS
+			$("#countWins").html(wins);
+			$("#countLives").html(lives);
+			$("#countGuesses").html(countGuesses);
+			$("#lettersGuessed").html(lettersGuessedArr.join(" "));
+
+			// CHOOSE RANDOM WORD
+			currentWord = words[Math.floor(Math.random() * words.length)];
+			this.changeHint();
+
+			// EMPTY ANSWER ARRAY TO STORE LETTERS
+			for (var i = 0; i < currentWord.word.length; i++) {
+				answer[i] = "_";
+			}
+			$("#currentWord").html(answer.join(" "));
+		},
+		loseMusic: function () {
+			var x = document.createElement("audio");
+			x.setAttribute("src", "assets/sounds/lose-music-2.mp3");
+			x.setAttribute("loop", "true");
+			x.play();
+		},
+		winMusic: function () {
+			var x = document.createElement("audio");
+			x.setAttribute("src", "assets/sounds/win-music.mp3");
+			x.setAttribute("loop", "true");
+			x.play();
+		},
+		roundOver: function () {
+			if (lives === 0) {
+				$("#gameplay").css("display", "none");
+				$("#gameover").css("display", "block");
+				$(".spaceman-gameover").css("display", "block");
+				themeSong.pause();
+				playVoice.play();
+				setTimeout(this.loseMusic, 300);
+			} else if (words.length === 0) {
+				$("#gameplay").css("display", "none");
+				$("#youwin").css("display", "block");
+				$(".spaceman-win").css("display", "block");
+				themeSong.pause();
+				setTimeout(this.winMusic, 50);
+			} else {
+				this.newWord();
+			}
+		},
+		blackHole: function () {
+			$(".spaceman-gameplay").animate({
+				left: "+=100",
+				top: "+=105",
+				opacity: "0"
+			}, 200, function () {
+				$(this).removeAttr("style");
+			});
+		},
+		changeBg: function () {
+			$(".background").css("background-image", "url(" + backgrounds[Math.floor(Math.random() * backgrounds.length)] + ")");
+
+		},
+		gameRound: function () {
+			document.onkeyup = function () {
+				var guess = String.fromCharCode(event.keyCode).toUpperCase();
+
+				// CHECK IF LETTER KEY IS PRESSED
+				if (letters.indexOf(guess) !== -1) {
+					if ((lettersMatched && lettersMatched.indexOf(guess) !== -1) || (lettersGuessed && lettersGuessed.indexOf(guess) !== -1)) {
+						$("#message").html(messages.alreadyGuessed);
+					} else if (currentWord.word.indexOf(guess) !== -1) {
+						for (var i = 0; i < currentWord.word.length; i++) {
+							if (currentWord.word[i] === guess) {
+								answer[i] = guess;
+								$("#currentWord").html(answer.join(" "));
+							}
+						}
+						for (var i = 0; i < currentWord.word.length; i++) {
+							if (currentWord.word.charAt(i) === guess) {
+								countLettersMatched += 1;
+							}
+						}
+						lettersMatched += guess;
+						if (countLettersMatched === currentWord.word.length) {
+							playCorrect.play();
+							wins++;
+							$("#message").html(messages.correct + " The mystery was " + currentWord.word);
+							$("#countWins").html(wins);
+
+							// REMOVE WORD AND IMAGE FROM ARRAY
+							words.splice(words.indexOf(currentWord), 1);
+							game.roundOver();
+						}
+					} else {
+						lettersGuessed += guess;
+						lettersGuessedArr.push(guess);
+						countGuesses -= 5;
+						$("#message").html(messages.incorrect);
+						$("#lettersGuessed").html(lettersGuessedArr.join(" "));
+						$("#countGuesses").html(countGuesses);
+						if (countGuesses === 0) {
+							lives--;
+							playIncorrect.play();
+							game.blackHole();
+							game.changeBg();
+							console.log(words);
+							$("#countLives").html(lives);
+							$("#message").html(messages.noMoreGuesses);
+							game.roundOver();
+						}
+					}
+				} else {
+					$("#message").html(messages.notLetter);
+				}
+			}
+		},
+	};
+
+	// SHOW GAME INSTRUCTIONS
 	$(window).on("load", function () {
 		$("#gameModal").modal("show");
 	});
 
-	// CHANGE BACKGROUND IMAGE
-	function changeBg() {
-		$(".background").css("background-image", "url(" + backgrounds[Math.floor(Math.random() * backgrounds.length)] + ")");
-	};
+	// GAMEPLAY
+	game.newWord();
+	game.roundOver();
+	game.gameRound();
 
-	// MOVE SPACEMAN THROUGH BLACK HOLE
-	function blackhole() {
-		$(".spaceman-gameplay").animate({
-			left: "+=100",
-			top: "+=105",
-			opacity: "0"
-		}, 200, function () {
-			$(this).removeAttr("style");
-		});
-	};
-
-	// RESET WORD AND GUESSES
-	function newWord() {
-		answer = [];
-		lettersGuessedArr = [];
-		lettersGuessed = lettersMatched = "";
-		countLettersMatched = 0;
-		countGuesses = 25;
-
-		// UPDATE STATS
-		$("#countWins").html(wins);
-		$("#countLives").html(lives);
-		$("#countGuesses").html(countGuesses);
-		$("#lettersGuessed").html(lettersGuessedArr.join(" "));
-
-		// CHOOSE RANDOM WORD AND ASSOCIATED IMAGE FROM words ARRAY
-		currentWord = words[Math.floor(Math.random() * words.length)];
-		console.log(currentWord);
-		console.log(words.indexOf(currentWord));
-
-		changeHint();
-
-		// EMPTY ANSWER ARRAY TO STORE LETTERS OF CURRENT WORD
-		for (var i = 0; i < currentWord.word.length; i++) {
-			answer[i] = "_";
-		}
-		$("#currentWord").html(answer.join(" "));
-	}
-
-	// UPDATE HINT IMAGE
-	function changeHint() {
-		$("#hintImage").attr("src", currentWord.img);
-	};
-
-	// GAME ROUND
-	function gameRound() {
-		document.onkeyup = function () {
-			var guess = String.fromCharCode(event.keyCode).toUpperCase();
-
-			// CHECK IF KEY PRESSED IS A LETTER
-			if (letters.indexOf(guess) !== -1) {
-				if ((lettersMatched && lettersMatched.indexOf(guess) !== -1) || (lettersGuessed && lettersGuessed.indexOf(guess) !== -1)) {
-					$("#message").html(messages.alreadyGuessed);
-				} else if (currentWord.word.indexOf(guess) !== -1) {
-					for (var i = 0; i < currentWord.word.length; i++) {
-						if (currentWord.word[i] === guess) {
-							answer[i] = guess;
-							$("#currentWord").html(answer.join(" "));
-						}
-					}
-					for (var i = 0; i < currentWord.word.length; i++) {
-						if (currentWord.word.charAt(i) === guess) {
-							countLettersMatched += 1;
-						}
-					}
-					lettersMatched += guess;
-					if (countLettersMatched === currentWord.word.length) {
-						playCorrect.play();
-						wins++;
-						$("#message").html(messages.correct + " The mystery was " + currentWord.word);
-						$("#countWins").html(wins);
-
-						// REMOVE WORD AND IMAGE FROM ARRAY AFTER GUESSED CORRECTLY
-						words.splice(words.indexOf(currentWord), 1);
-						roundOver();
-					}
-				} else {
-					lettersGuessed += guess;
-					lettersGuessedArr.push(guess);
-					countGuesses -= 5;
-					$("#message").html(messages.incorrect);
-					$("#lettersGuessed").html(lettersGuessedArr.join(" "));
-					$("#countGuesses").html(countGuesses);
-					if (countGuesses === 0) {
-						lives--;
-						playIncorrect.play();
-						blackhole();
-						changeBg();
-						console.log(words);
-						$("#countLives").html(lives);
-						$("#message").html(messages.noMoreGuesses);
-						roundOver();
-					}
-				}
-			} else {
-				$("#message").html(messages.notLetter);
-			}
-		};
-	};
-	// END GAME ROUND
-
-	// CHECK WHETHER TO CONTINUE GAME OR END GAME
-	function roundOver() {
-		if (lives === 0) {
-			$("#gameplay").css("display", "none");
-			$("#gameover").css("display", "block");
-			$(".spaceman-gameover").css("display", "block");
-			playVoice.play();
-			setTimeout(loseMusic, 2000);
-		} else if (words.length === 0) {
-			$("#gameplay").css("display", "none");
-			$("#youwin").css("display", "block");
-			$(".spaceman-win").css("display", "block");
-			playYell.play();
-			setTimeout(winMusic, 2000);
-		} else {
-			newWord();
-		}
-	};
-	// END CHECK
-
-	// RELOAD PAGE WHEN BUTTON CLICKED
+	// RELOAD PAGE TO PLAY AGAIN
 	$(".playAgain").click(function () {
 		location.reload();
 	});
-
-	// INVOKE FUNCTIONS TO PLAY GAME
-	newWord();
-	gameRound();
 });
